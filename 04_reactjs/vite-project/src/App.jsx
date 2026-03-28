bcaa94e324f1e576c73923f8328040f501579707
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2026-03-28 21:02:59 +0800

    Add initial project setup with React, Vite, and basic components
    - Created Car component to display car details (make, model, year, price).
    - Added index.css for global styles.
    - Set up main.jsx for rendering the App component.
    - Configured Vite with a proxy for API requests.
    - Included SVG assets for React and Vite logos.

---
+++ 04_reactjs/vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Car from './components/Car'
+import { useState } from 'react'
+import { useEffect } from 'react';
+
+const App = () => {
+
+  const [cars, setCars] = useState([]);
+
+  useEffect(() =>{
+    fetch('api/v1/cars').
+      then(res => res.json()).
+      then(data => setCars(data)).
+      catch(err => console.log(err));
+
+    }, [])
+
+  return (
+    <div>
+      <h1>Welcome to the car store</h1>
+      <ul>
+        {cars.map(car => (
+          <Car key={car.id} {... car} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
